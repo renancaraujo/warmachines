b9546baedb8dde5ea8a3d25d1f37627c7e6725c6
Renan Araujo <renan.araujo@dextra-sw.com>
2019-02-11 17:57:41 -0200

    user gestures

--- app/lib/routes/tank/tank_screen.dart
+++ app/lib/routes/tank/tank_screen.dart
@@ -123,110 +123,180 @@ class _TankInfo extends StatefulWidget {
 
 class _TankInfoState extends State<_TankInfo>
     with TickerProviderStateMixin, AfterLayoutMixin<_TankInfo> {
-  AnimationController offsetAnimationController;
-  Animation<double> offsetAnimation;
-  double offset;
+  AnimationController yOffsetAnimationController;
+  Animation<double> yOffsetAnimation;
+
+  AnimationController xOffsetAnimationController;
+  Animation<double> xOffsetAnimation;
+
+  double yOffset;
+  double xOffset;
+
+  double xOffsetStart;
+
   final double initial = 50;
 
-  void handleOffsetAnimation() {
+  void handleYOffsetAnimation() {
     setState(() {
-      offset = offsetAnimation.value;
+      yOffset = yOffsetAnimation.value;
+    });
+  }
+
+  void handleXOffsetAnimation() {
+    setState(() {
+      xOffset = xOffsetAnimation.value;
     });
   }
 
   @override
   void initState() {
-    offset = initial;
     super.initState();
-    offsetAnimationController = AnimationController(vsync: this)
-      ..addListener(handleOffsetAnimation);
+    xOffset = 0;
+    yOffset = initial;
+    yOffsetAnimationController = AnimationController(vsync: this)
+      ..addListener(handleYOffsetAnimation);
+    xOffsetAnimationController = AnimationController(vsync: this)
+      ..addListener(handleXOffsetAnimation);
   }
 
   @override
   void afterFirstLayout(BuildContext context) {
-    offsetAnimation = Tween<double>(begin: initial, end: 0).animate(
+    yOffsetAnimation = Tween<double>(begin: initial, end: 0).animate(
         CurvedAnimation(
-            parent: offsetAnimationController, curve: Curves.easeOutExpo));
-    offsetAnimationController
+            parent: yOffsetAnimationController, curve: Curves.easeOutExpo));
+    yOffsetAnimationController
       ..value = 0.0
       ..duration = Duration(milliseconds: 1350)
       ..forward();
   }
 
+  TickerFuture animateX(double to){
+    xOffsetAnimation = Tween<double>(begin: xOffset, end: to).animate(
+        CurvedAnimation(
+            parent: xOffsetAnimationController, curve: Curves.easeOutSine));
+    xOffsetAnimationController
+      ..value = 0.0
+      ..duration = Duration(milliseconds: 350);
+    return xOffsetAnimationController.forward();
+  }
+
   @override
   void dispose() {
-    offsetAnimationController.dispose();
+    yOffsetAnimationController.dispose();
+    xOffsetAnimationController.dispose();
     super.dispose();
+  }
+
+  void onHorizontalDragStart(DragStartDetails details){
+    xOffsetAnimationController.stop();
+    xOffsetStart = xOffset;
+  }
+
+  void onHorizontalDragUpdate(DragUpdateDetails details){
+    double nextX = xOffset + details.delta.dx;
+    if(nextX < 0){
+      return;
+    }
+    setState(() {
+      xOffset = nextX;
+    });
+  }
+
+  void onHorizontalDragEnd(DragEndDetails details) async{
+    final double halfScreen = MediaQuery.of(context).size.width * 0.35;
+    if(xOffset < halfScreen){
+      animateX(0);
+      return;
+    }
+    Application.instance.router.pop(context);
+  }
+  void onHorizontalDragCancel(){
+    animateX(0);
   }
 
   @override
   Widget build(BuildContext context) {
-    Matrix4 matrix = Matrix4.identity()..translate(0.0, offset);
-    double opacity = ((offset - initial).abs() / initial);
+
+    final double swipeFactor = (1.0 - (xOffset / (MediaQuery.of(context).size.width * 0.65))).clamp(0.0, 1.0);
+    Matrix4 innerMatrix = Matrix4.identity()..translate(xOffset, yOffset);
+    double innerOpacity = swipeFactor;
+
+    Matrix4 outerMatrix = Matrix4.identity()..translate(0.0, yOffset + ( swipeFactor * 20.0 )) ;
+    double outerOpacity = ((yOffset - initial).abs() / initial).clamp(0.0, innerOpacity*1.5).clamp(0.0, 1.0);
+
 
     return LayoutBuilder(
         builder: (BuildContext context, BoxConstraints constraints) {
-      return Transform(
-          transform: matrix,
-          child: Opacity(
-            opacity: opacity,
-            child: Container(
-                width: constraints.maxWidth,
-                padding: EdgeInsets.all(16.0),
-                decoration: BoxDecoration(
-                    gradient: LinearGradient(
-                        begin: Alignment.topCenter,
-                        end: Alignment.bottomCenter,
-                        stops: [
-                      0.0,
-                      0.9
-                    ],
-                        colors: [
-                      Colors.black.withAlpha(0),
-                      Colors.black,
-                    ])),
-                child: Transform(
-                  transform: matrix,
-                  child: ListView(
-                    controller: widget.scrollController,
-                    padding: EdgeInsets.only(
-                        top: MediaQuery.of(context).size.height - 250),
-                    physics: BouncingScrollPhysics(),
-                    children: <Widget>[
-                      Text(
-                        widget.tank.name,
-                        style: TextStyle(
-                            fontSize: 72.0,
-                            fontWeight: FontWeight.w800,
-                            letterSpacing: 2.0),
-                      ),
-                      Container(
-                        margin: EdgeInsets.symmetric(vertical: 20.0),
-                        child: Row(
-                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                          children: <Widget>[
-                            Text(widget.tank.nation.name),
-                            Text("${widget.tank.period.name}")
+      return GestureDetector(
+        onHorizontalDragStart: onHorizontalDragStart,
+        onHorizontalDragUpdate: onHorizontalDragUpdate,
+        onHorizontalDragEnd: onHorizontalDragEnd,
+        onHorizontalDragCancel: onHorizontalDragCancel,
+        child: Transform(
+            transform: outerMatrix,
+            child: Opacity(
+              opacity: outerOpacity,
+              child: Container(
+                  width: constraints.maxWidth,
+                  padding: EdgeInsets.all(16.0),
+                  decoration: BoxDecoration(
+                      gradient: LinearGradient(
+                          begin: Alignment.topCenter,
+                          end: Alignment.bottomCenter,
+                          stops: [
+                            0.0,
+                            0.9
                           ],
-                        ),
+                          colors: [
+                            Colors.black.withAlpha(0),
+                            Colors.black,
+                          ])),
+                  child: Transform(
+                    transform: innerMatrix,
+                    child: Opacity(
+                      opacity: innerOpacity,
+                      child: ListView(
+                        controller: widget.scrollController,
+                        padding: EdgeInsets.only(
+                            top: MediaQuery.of(context).size.height - 250),
+                        physics: BouncingScrollPhysics(),
+                        children: <Widget>[
+                          Text(
+                            widget.tank.name,
+                            style: TextStyle(
+                                fontSize: 72.0,
+                                fontWeight: FontWeight.w800,
+                                letterSpacing: 2.0),
+                          ),
+                          Container(
+                            margin: EdgeInsets.symmetric(vertical: 20.0),
+                            child: Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                              children: <Widget>[
+                                Text(widget.tank.nation.name),
+                                Text("${widget.tank.period.name}")
+                              ],
+                            ),
+                          ),
+                          Container(
+                            height: 1.0,
+                            decoration: BoxDecoration(color: Colors.white),
+                            margin: EdgeInsets.only(top: 20, bottom: 50),
+                          ),
+                          Container(
+                            margin: EdgeInsets.only(bottom: 400),
+                            child: Text(
+                              widget.tank.description,
+                              style: TextStyle(
+                                  letterSpacing: 1.0, fontSize: 19.0, height: 1.2),
+                            ),
+                          )
+                        ],
                       ),
-                      Container(
-                        height: 1.0,
-                        decoration: BoxDecoration(color: Colors.white),
-                        margin: EdgeInsets.only(top: 20, bottom: 50),
-                      ),
-                      Container(
-                        margin: EdgeInsets.only(bottom: 400),
-                        child: Text(
-                          widget.tank.description,
-                          style: TextStyle(
-                              letterSpacing: 1.0, fontSize: 19.0, height: 1.2),
-                        ),
-                      )
-                    ],
-                  ),
-                )),
-          ));
+                    ),
+                  )),
+            )),
+      );
     });
   }
 }
